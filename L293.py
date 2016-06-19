b8f6009ba07817a22b81fea785c33ede184305f0
Peradat Permjai <gullwings@msn.com>
2016-06-20 02:35:38 +0700

    matpin

--- L293.py
+++ L293.py
@@ -5,43 +5,39 @@ GPIO.setmode(GPIO.BOARD)
 
 class Motor:
 
-    def __init__(self, pinForward, pinBackward, pinControl):
+    def __init__(self, pinForward, pinBackward, pinEn):
         """ Initialize the motor with its control pins and start pulse-width
              modulation """
 
-        self.pinForward = pinForward
-        self.pinBackward = pinBackward
-        self.pinControl = pinControl
-        GPIO.setup(self.pinForward, GPIO.OUT)
-        GPIO.setup(self.pinBackward, GPIO.OUT)
+        self.pin1 = pinForward
+        self.pin2 = pinBackward
+        self.pinEn = pinEn
+        GPIO.setup(self.pin1, GPIO.OUT)
+        GPIO.setup(self.pin2, GPIO.OUT)
         GPIO.setup(self.pinControl, GPIO.OUT)
-        self.pwm_forward = GPIO.PWM(self.pinForward, 100)
-        self.pwm_backward = GPIO.PWM(self.pinBackward, 100)
-        self.pwm_forward.start(0)
-        self.pwm_backward.start(0)
-        GPIO.output(self.pinControl,GPIO.HIGH) 
+        self.pwm = GPIO.PWM(self.pinEn, 100)
+        self.pwm.start(0)
+        GPIO.output(self.pin1,GPIO.LOW)
+        GPIO.output(self.pin2,GPIO.LOW)  
 
     def forward(self, speed):
-        """ pinForward is the forward Pin, so we change its duty
-             cycle according to speed. """
-        self.pwm_backward.ChangeDutyCycle(0)
-        self.pwm_forward.ChangeDutyCycle(speed)    
+        GPIO.output(self.pin1,GPIO.LOW)
+        GPIO.output(self.pin2,GPIO.HIGH)      
+        self.pwm.ChangeDutyCycle(speed)
+         
 
     def backward(self, speed):
-        """ pinBackward is the forward Pin, so we change its duty
-             cycle according to speed. """
-
-        self.pwm_forward.ChangeDutyCycle(0)
-        self.pwm_backward.ChangeDutyCycle(speed)
+        GPIO.output(self.pin1,GPIO.HIGH)
+        GPIO.output(self.pin2,GPIO.LOW)      
+        self.pwm.ChangeDutyCycle(speed)
 
     def stop(self):
-        """ Set the duty cycle of both control pins to zero to stop the motor. """
+        GPIO.output(self.pin1,GPIO.LOW)
+        GPIO.output(self.pin2,GPIO.LOW)      
+        self.pwm.ChangeDutyCycle(0)
 
-        self.pwm_forward.ChangeDutyCycle(0)
-        self.pwm_backward.ChangeDutyCycle(0)
-
-motor1 = Motor(16, 22, 18)
-motor2 = Motor(23, 19, 21)
+motor1 = Motor(18, 22, 16)
+motor2 = Motor(21, 23, 19)
 
 # Motor 1 test
 motor1.forward(90)
