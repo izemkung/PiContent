2763c84cdbd18de2f3153bb8996c577883c41abd
Peradat Permjai <gullwings@msn.com>
2016-06-20 21:59:25 +0700

    thingspeak

---
+++ thingspeak.py
@@ -0,0 +1,18 @@
+import httplib, urllib
+key = 'J9MJRX01WZHIOXRW'
+
+#Calculate CPU temperature of Raspberry Pi in Degrees C
+temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3 # Get Raspberry Pi CPU temp
+params = urllib.urlencode({'field1': temp, 'key':key }) 
+headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+conn = httplib.HTTPConnection("api.thingspeak.com:80")
+try:
+    conn.request("POST", "/update", params, headers)
+    response = conn.getresponse()
+    print temp
+    print response.status, response.reason
+    data = response.read()
+    conn.close()
+except:
+    print "connection failed"
+break
